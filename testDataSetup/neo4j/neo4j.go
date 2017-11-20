205e6a23b8db747d5b384f2538f6fc15ce6b75dd
Matt Guest <mattguest1988@gmail.com>
2017-11-20 15:35:12 +0000

    Initial setup and teardown for neo4j

--- testDataSetup/neo4j/neo4j.go
+++ testDataSetup/neo4j/neo4j.go
@@ -1,23 +1,60 @@
 package neo4j
 
-import "github.com/ONSdigital/dp-api-tests/config"
+import (
+	"bytes"
+	"fmt"
+	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+	"html/template"
+)
+
+const ObservationTestData = "testDataSetup/neo4j/data.cypher"
+
+type Datastore struct {
+	instance   string
+	testData   string
+	connection bolt.Conn
+}
+
+type CypherTemplate struct {
+	Instance string
+}
+
+func NewDatastore(uri, instance, testdata string) *Datastore {
+	driver, err := bolt.NewDriver().OpenNeo(uri)
+	if err != nil {
+		panic(err)
+	}
+	return &Datastore{connection: driver, instance: instance, testData: testdata}
+}
 
 // Teardown ...
-func Teardown() error {
-  config, err := config.Get()
+func (ds *Datastore) TeardownObservation() error {
+	query := fmt.Sprintf("MATCH (n:`_%s_Instance`)-[r]-(m)-[t]-(o) detach delete n,m,o;", ds.instance)
+	_, err := ds.connection.QueryNeo(query, nil)
 	if err != nil {
 		return err
 	}
-
-	return nil
+	return ds.connection.Close()
 }
 
 // Setup ...
-func Setup() error {
-  config, err := config.Get()
+func (ds *Datastore) Setup() error {
+	t, err := template.ParseFiles("testDataSetup/neo4j/data.cypher")
+	if err != nil {
+		return err
+	}
+	query := new(bytes.Buffer)
+	t.Execute(query, CypherTemplate{Instance: ds.instance})
+	_, err = ds.connection.QueryNeo(query.String(), nil)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	return err
 }
+
+//func main() {
+//	ds := NewDatastore("bolt://localhost:7687", "123-234", ObservationTestData)
+//	ds.Setup()
+//	ds.TeardownObservation()
+//}
