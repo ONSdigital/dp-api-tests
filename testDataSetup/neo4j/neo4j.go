d8835ca4fdfe0f899c32e4034b4f06fbbdf84941
Matt Guest <mattguest1988@gmail.com>
2017-11-27 15:12:16 +0000

    setup and teardown for neo4j

--- testDataSetup/neo4j/neo4j.go
+++ testDataSetup/neo4j/neo4j.go
@@ -7,18 +7,22 @@ import (
 	"html/template"
 )
 
-const ObservationTestData = "testDataSetup/neo4j/data.cypher"
+const ObservationTestData = "../testDataSetup/neo4j/instance.cypher"
+const HierarchyTestData = "testDataSetup/neo4j/hierarchy.cypher"
 
+// Datastore used to setup data within neo4j
 type Datastore struct {
 	instance   string
 	testData   string
 	connection bolt.Conn
 }
 
+// CypherTemplate allows cypher queries to be updated with new ID
 type CypherTemplate struct {
 	Instance string
 }
 
+// NewDatastore creates a new datastore for a test
 func NewDatastore(uri, instance, testdata string) *Datastore {
 	driver, err := bolt.NewDriver().OpenNeo(uri)
 	if err != nil {
@@ -27,34 +31,42 @@ func NewDatastore(uri, instance, testdata string) *Datastore {
 	return &Datastore{connection: driver, instance: instance, testData: testdata}
 }
 
-// Teardown ...
-func (ds *Datastore) TeardownObservation() error {
+// TeardownInstance removes all instance nodes within neo4j
+func (ds *Datastore) TeardownInstance() error {
 	query := fmt.Sprintf("MATCH (n:`_%s_Instance`)-[r]-(m)-[t]-(o) detach delete n,m,o;", ds.instance)
-	_, err := ds.connection.QueryNeo(query, nil)
+	results, err := ds.connection.QueryNeo(query, nil)
+	if err != nil {
+		return err
+	}
+	results.Close()
+	return ds.connection.Close()
+}
+
+// TeardownHierarchy removes all hierarchy nodes within neo4j
+func (ds *Datastore) TeardownHierarchy() error {
+	_, err := ds.connection.ExecNeo("MATCH (n:`_generic_hierarchy_node_e44de4c4-d39e-4e2f-942b-3ca10584d078`) DETACH DELETE n", nil)
+	if err != nil {
+		return err
+	}
+	query := fmt.Sprintf("MATCH (n:`_hierarchy_node_%s_aggregate`) DETACH DELETE n", ds.instance)
+	_, err = ds.connection.ExecNeo(query, nil)
 	if err != nil {
 		return err
 	}
 	return ds.connection.Close()
 }
 
-// Setup ...
+// Setup the neo4j database
 func (ds *Datastore) Setup() error {
-	t, err := template.ParseFiles("testDataSetup/neo4j/data.cypher")
+	t, err := template.ParseFiles(ds.testData)
 	if err != nil {
 		return err
 	}
 	query := new(bytes.Buffer)
 	t.Execute(query, CypherTemplate{Instance: ds.instance})
-	_, err = ds.connection.QueryNeo(query.String(), nil)
+	_, err = ds.connection.ExecNeo(query.String(), nil)
 	if err != nil {
 		return err
 	}
-
 	return err
 }
-
-//func main() {
-//	ds := NewDatastore("bolt://localhost:7687", "123-234", ObservationTestData)
-//	ds.Setup()
-//	ds.TeardownObservation()
-//}
