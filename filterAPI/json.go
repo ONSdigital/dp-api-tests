55588fc9c53d49fd6b77e6635ee8249c987e7308
Eleanor Deal <eleanor.deal@gmail.com>
2018-02-07 17:41:46 +0000

    Add tests for authenticated filter creation

--- filterAPI/json.go
+++ filterAPI/json.go
@@ -40,6 +40,40 @@ func GetValidPublishedInstanceDataBSON(instanceID string) bson.M {
 	}
 }
 
+func GetUnpublishedInstanceDataBSON(instanceID string) bson.M {
+	return bson.M{
+		"$set": bson.M{
+			"_id":                   instanceID,
+			"collection_id":         "108064B3-A808-449B-9041-EA3A2F72CFAA",
+			"downloads.csv.url":     "http://localhost:8080/aws/census-2017-1-csv",
+			"downloads.csv.size":    "10mb",
+			"downloads.xls.url":     "http://localhost:8080/aws/census-2017-1-xls",
+			"downloads.xls.size":    "24mb",
+			"edition":               "2017",
+			"headers":               []string{"time", "geography"},
+			"id":                    instanceID,
+			"last_updated":          "2017-09-08", // TODO Should be isodate
+			"license":               "ONS License",
+			"links.job.id":          "042e216a-7822-4fa0-a3d6-e3f5248ffc35",
+			"links.job.href":        "http://localhost:8080/jobs/042e216a-7822-4fa0-a3d6-e3f5248ffc35",
+			"links.dataset.id":      "123",
+			"links.dataset.href":    "http://localhost:8080/datasets/123",
+			"links.dimensions.href": "http://localhost:8080/datasets/123/editions/2017/versions/1/dimensions",
+			"links.edition.id":      "2017",
+			"links.edition.href":    "http://localhost:8080/datasets/123/editions/2017",
+			"links.self.href":       "http://localhost:8080/instances/" + instanceID,
+			"links.version.href":    "http://localhost:8080/datasets/123/editions/2017/versions/1",
+			"links.version.id":      "1",
+			"release_date":          "2017-12-12", // TODO Should be isodate
+			"state":                 "associated",
+			"total_inserted_observations": 1000,
+			"total_observations":          1000,
+			"version":                     1,
+			"test_data":                   "true",
+		},
+	}
+}
+
 type Dimension struct {
 	URL     string   `bson:"dimension_url"`
 	Name    string   `bson:"name"`
