a0bb8ea68820182bc44d96b01c61f111d993e2c0
nshumoogum <nathan.shumoogum@methods.co.uk>
2018-02-26 12:46:34 +0000

    Extend new search API endpoint Create index

---
+++ searchAPI/put_search_index_test.go
@@ -0,0 +1,70 @@
+package searchAPI
+
+import (
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/ONSdigital/dp-api-tests/testDataSetup/elasticsearch"
+	"github.com/ONSdigital/go-ns/log"
+	"github.com/gavv/httpexpect"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSuccessfullyCreateSearchIndex(t *testing.T) {
+	searchAPI := httpexpect.New(t, cfg.SearchAPIURL)
+	dimension := "aggregate"
+
+	Convey("Given an elasticsearch index does not exists for an instance dimension", t, func() {
+		Convey("When a PUT request is made to search API with valid authentication header", func() {
+			Convey("Then a message is sent to kafka to create an index and the response returns status ok (200)", func() {
+
+				searchAPI.PUT("/search/instances/{instanceID}/dimensions/{dimension}", instanceID, "aggregate").
+					WithHeader(internalTokenHeader, internalTokenID).Expect().Status(http.StatusOK)
+			})
+		})
+	})
+
+	// Eventhough kafka message is created, the index will never be created as the
+	// instance id will not match any instance that exists against an environment/local
+	// database, but just in the unlikely scenario this does create an index, delete it
+	deleteIndex(instanceID, dimension)
+}
+
+func TestFailToCreateSearchIndex(t *testing.T) {
+	searchAPI := httpexpect.New(t, cfg.SearchAPIURL)
+	dimension := "aggregate"
+
+	Convey("Given an elasticsearch index does not exists for an instance dimension", t, func() {
+		deleteIndex(instanceID, dimension)
+
+		Convey("When a PUT request is made to search API without an authentication header", func() {
+			Convey("Then the response returns status not found (401)", func() {
+
+				searchAPI.PUT("/search/instances/{instanceID}/dimensions/{dimension}", instanceID, "aggregate").
+					Expect().Status(http.StatusUnauthorized).Body().Contains("No authentication header provided\n")
+			})
+		})
+
+		Convey("When a PUT request is made to search API with Invalid authentication header", func() {
+			Convey("Then the response returns status unauthorised (401)", func() {
+
+				searchAPI.PUT("/search/instances/{instanceID}/dimensions/{dimension}", instanceID, "aggregate").
+					WithHeader(internalTokenHeader, "grey").Expect().Status(http.StatusUnauthorized).Body().Contains("Unauthorised access to API\n")
+			})
+		})
+	})
+
+	deleteIndex(instanceID, dimension)
+}
+
+func deleteIndex(ID, dimension string) {
+	path := cfg.ElasticSearchAPIURL + "/" + ID + "_" + dimension
+	statusCode, err := elasticsearch.DeleteIndex(path)
+	if err != nil {
+		if statusCode != http.StatusNotFound {
+			log.ErrorC("failed to delete index", err, log.Data{"path": path})
+			os.Exit(1)
+		}
+	}
+}
