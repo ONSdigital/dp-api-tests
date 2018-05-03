792a970ada21ff22186b9a0eff8ce1e1efa52065
nshumoogum <nathan.shumoogum@methods.co.uk>
2018-05-03 07:50:34 +0100

    Check multiple dimensions missing when calling dataset observations endpoint
    Also break up lines to make them more readable and remove trailing
    comments

--- datasetAPI/get_observation_test.go
+++ datasetAPI/get_observation_test.go
@@ -58,7 +58,8 @@ func TestSuccessfullyGetObservationForVersion(t *testing.T) {
 			Convey("Then the response body contains the expected observation data", func() {
 
 				response := datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
-					WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").WithHeader(florenceTokenName, florenceToken).
+					WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").
+					WithHeader(florenceTokenName, florenceToken).
 					Expect().Status(http.StatusOK).JSON().Object()
 
 				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Length().Equal(1)
@@ -72,8 +73,7 @@ func TestSuccessfullyGetObservationForVersion(t *testing.T) {
 				response.Value("dimensions").Object().Value("time").Object().Value("options").Array().Element(0).Object().Value("id").Equal("Aug-16")
 				response.Value("observation").Equal("154.6")
 				response.Value("links").Object().Value("dataset_metadata").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/2/metadata$")
-				response.Value("links").Object().Value("self").Object().Value("href").String().Match(".+/datasets/" + datasetID + "/editions/" + edition + "/versions/2/observations\\?aggregate=cpi1dim1S40403&geography=K02000001&time=Aug-16$") // ?aggregate=cpi1dim1S40403&geography=K02000001&time=Aug-16")
-				response.Value("links").Object().Value("version").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/2$")
+				response.Value("links").Object().Value("self").Object().Value("href").String().Match(".+/datasets/" + datasetID + "/editions/" + edition + "/versions/2/observations\\?aggregate=cpi1dim1S40403&geography=K02000001&time=Aug-16$")
 				response.Value("links").Object().Value("version").Object().Value("id").Equal("2")
 				response.Value("unit_of_measure").Equal("Pounds Sterling")
 			})
@@ -82,7 +82,8 @@ func TestSuccessfullyGetObservationForVersion(t *testing.T) {
 		Convey("When an unauthenticated request is made to get an observation resource for a published version", func() {
 			Convey("Then the response body contains the expected observation data", func() {
 				response := datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/1/observations", datasetID, edition).
-					WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1G50100").Expect().Status(http.StatusOK).JSON().Object()
+					WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1G50100").
+					Expect().Status(http.StatusOK).JSON().Object()
 
 				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Length().Equal(1)
 				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Element(0).Object().Value("href").String().Match("/codelists/508064B3-A808-449B-9041-EA3A2F72CFAD/codes/cpi1dim1G50100$")
@@ -95,7 +96,7 @@ func TestSuccessfullyGetObservationForVersion(t *testing.T) {
 				response.Value("dimensions").Object().Value("time").Object().Value("options").Array().Element(0).Object().Value("id").Equal("Aug-16")
 				response.Value("observation").Equal("117.9")
 				response.Value("links").Object().Value("dataset_metadata").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/1/metadata$")
-				response.Value("links").Object().Value("self").Object().Value("href").String().Match(".+/datasets/" + datasetID + "/editions/" + edition + "/versions/1/observations\\?aggregate=cpi1dim1G50100&geography=K02000001&time=Aug-16$") // ?aggregate=cpi1dim1S40403&geography=K02000001&time=Aug-16")
+				response.Value("links").Object().Value("self").Object().Value("href").String().Match(".+/datasets/" + datasetID + "/editions/" + edition + "/versions/1/observations\\?aggregate=cpi1dim1G50100&geography=K02000001&time=Aug-16$")
 				response.Value("links").Object().Value("version").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/1$")
 				response.Value("links").Object().Value("version").Object().Value("id").Equal("1")
 				response.Value("unit_of_measure").Equal("Pounds Sterling")
@@ -149,8 +150,10 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 		Convey("When an authorised request to get an observation for a version of a dataset", func() {
 			Convey("Then return status not found (404) with message `Dataset not found`", func() {
 				datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
-					WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").WithHeader(florenceTokenName, florenceToken).
-					Expect().Status(http.StatusNotFound).Body().Contains("Dataset not found")
+					WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").
+					WithHeader(florenceTokenName, florenceToken).
+					Expect().Status(http.StatusNotFound).
+					Body().Contains("Dataset not found")
 			})
 		})
 	})
@@ -189,8 +192,10 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 			Convey("When a request to get an observation for a version of a dataset", func() {
 				Convey("Then return status not found (404) with message `Edition not found`", func() {
 					datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
-						WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").WithHeader(florenceTokenName, florenceToken).
-						Expect().Status(http.StatusNotFound).Body().Contains("Edition not found")
+						WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").
+						WithHeader(florenceTokenName, florenceToken).
+						Expect().Status(http.StatusNotFound).
+						Body().Contains("Edition not found")
 				})
 			})
 		})
@@ -206,8 +211,10 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 				Convey("When a request to get an observation for a version of a dataset", func() {
 					Convey("Then return status not found (404) with message `Version not found`", func() {
 						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
-							WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").WithHeader(florenceTokenName, florenceToken).
-							Expect().Status(http.StatusNotFound).Body().Contains("Version not found")
+							WithQueryString("time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").
+							WithHeader(florenceTokenName, florenceToken).
+							Expect().Status(http.StatusNotFound).
+							Body().Contains("Version not found")
 					})
 				})
 			})
@@ -229,25 +236,31 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 
 				Convey("When a request to get an observation for a version of a dataset with incorrect query parameters", func() {
 					Convey("Then return status bad request (400) with a message", func() {
-						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).WithHeader(florenceTokenName, florenceToken).
-							WithQueryString("age=24&time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").
-							Expect().Status(http.StatusBadRequest).Body().Contains("Incorrect selection of query parameters: [age], these dimensions do not exist for this version of the dataset")
+						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
+							WithHeader(florenceTokenName, florenceToken).
+							WithQueryString("age=24&gender=male&time=Aug-16&geography=K02000001&aggregate=cpi1dim1S40403").
+							Expect().Status(http.StatusBadRequest).
+							Body().Contains("Incorrect selection of query parameters: [age gender], these dimensions do not exist for this version of the dataset")
 					})
 				})
 
 				Convey("When a request to get an observation for a version of a dataset with missing query parameters", func() {
 					Convey("Then return status bad request (400) with a message", func() {
-						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).WithHeader(florenceTokenName, florenceToken).
-							WithQueryString("geography=K02000001&aggregate=cpi1dim1S40403").
-							Expect().Status(http.StatusBadRequest).Body().Contains("Missing query parameters for the following dimensions: [time]")
+						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
+							WithHeader(florenceTokenName, florenceToken).
+							WithQueryString("geography=K02000001").
+							Expect().Status(http.StatusBadRequest).
+							Body().Contains("Missing query parameters for the following dimensions: [time aggregate]")
 					})
 				})
 
 				Convey("When a request to get an observation for a version of a dataset with the correct query parameters but the values don't exist", func() {
 					Convey("Then return status not found (404) with message `Observation not found`", func() {
-						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).WithHeader(florenceTokenName, florenceToken).
+						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
+							WithHeader(florenceTokenName, florenceToken).
 							WithQueryString("time=Aug-17&geography=K02000001&aggregate=cpi1dim1S40403").
-							Expect().Status(http.StatusNotFound).Body().Contains("Observation not found")
+							Expect().Status(http.StatusNotFound).
+							Body().Contains("Observation not found")
 					})
 				})
 			})
