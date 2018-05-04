df75ab52aa88f61847abf5ba074ddfa072909f46
Carl Hembrough <carl@bitwaves.co.uk>
2018-05-04 11:47:22 +0100

    Add healthcheck tests to the filter API tests

---
+++ publishing/filterAPI/get_healthcheck_test.go
@@ -0,0 +1,30 @@
+package filterAPI
+
+import (
+"testing"
+
+"github.com/gavv/httpexpect"
+. "github.com/smartystreets/goconvey/convey"
+"net/http"
+)
+
+func TestHealthcheck(t *testing.T) {
+
+	filterAPI := httpexpect.New(t, cfg.FilterAPIURL)
+
+	Convey("Given a request with no authentication headers", t, func() {
+		Convey("When get healthcheck is called", func() {
+			Convey("Then the response returns a status of either 200 or 429", func() {
+
+				response := filterAPI.
+					GET("/healthcheck").
+					Expect().Raw()
+
+				isValidResponse := response.StatusCode == http.StatusOK ||
+					response.StatusCode == http.StatusTooManyRequests
+
+				So(isValidResponse, ShouldBeTrue)
+			})
+		})
+	})
+}
