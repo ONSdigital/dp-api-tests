de9114ba283e7c75ecb272eed12d4ae88d7d69a8
nshumoogum <nathan.shumoogum@methods.co.uk>
2018-05-08 13:47:16 +0100

    Update observations endpoint tests

--- publishing/datasetAPI/get_observation_test.go
+++ publishing/datasetAPI/get_observation_test.go
@@ -63,20 +63,21 @@ func TestSuccessfullyGetObservationForVersion(t *testing.T) {
 					WithHeader(florenceTokenName, florenceToken).
 					Expect().Status(http.StatusOK).JSON().Object()
 
-				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Length().Equal(1)
-				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Element(0).Object().Value("href").String().Match("/codelists/508064B3-A808-449B-9041-EA3A2F72CFAD/codes/cpi1dim1G50100$")
-				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Element(0).Object().Value("id").Equal("cpi1dim1G50100")
-				response.Value("dimensions").Object().Value("geography").Object().Value("options").Array().Length().Equal(1)
-				response.Value("dimensions").Object().Value("geography").Object().Value("options").Array().Element(0).Object().Value("href").String().Match("/codelists/708064B3-A808-449B-9041-EA3A2F72CFAF/codes/K02000001$")
-				response.Value("dimensions").Object().Value("geography").Object().Value("options").Array().Element(0).Object().Value("id").Equal("K02000001")
-				response.Value("dimensions").Object().Value("time").Object().Value("options").Array().Length().Equal(1)
-				response.Value("dimensions").Object().Value("time").Object().Value("options").Array().Element(0).Object().Value("href").String().Match("/codelists/608064B3-A808-449B-9041-EA3A2F72CFAE/codes/Aug-16$")
-				response.Value("dimensions").Object().Value("time").Object().Value("options").Array().Element(0).Object().Value("id").Equal("Aug-16")
-				response.Value("observation").Equal("117.9")
+				response.Value("dimensions").Object().Value("aggregate").Object().Value("option").Object().Value("href").String().Match("/codelists/508064B3-A808-449B-9041-EA3A2F72CFAD/codes/cpi1dim1G50100$")
+				response.Value("dimensions").Object().Value("aggregate").Object().Value("option").Object().Value("id").Equal("cpi1dim1G50100")
+				response.Value("dimensions").Object().Value("geography").Object().Value("option").Object().Value("href").String().Match("/codelists/708064B3-A808-449B-9041-EA3A2F72CFAF/codes/K02000001$")
+				response.Value("dimensions").Object().Value("geography").Object().Value("option").Object().Value("id").Equal("K02000001")
+				response.Value("dimensions").Object().Value("time").Object().Value("option").Object().Value("href").String().Match("/codelists/608064B3-A808-449B-9041-EA3A2F72CFAE/codes/Aug-16$")
+				response.Value("dimensions").Object().Value("time").Object().Value("option").Object().Value("id").Equal("Aug-16")
+				response.Value("limit").Equal(10000)
 				response.Value("links").Object().Value("dataset_metadata").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/1/metadata$")
 				response.Value("links").Object().Value("self").Object().Value("href").String().Match(".+/datasets/" + datasetID + "/editions/" + edition + "/versions/1/observations\\?aggregate=cpi1dim1G50100&geography=K02000001&time=Aug-16$")
 				response.Value("links").Object().Value("version").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/1$")
 				response.Value("links").Object().Value("version").Object().Value("id").Equal("1")
+				response.Value("observations").Array().Length().Equal(1)
+				response.Value("observations").Array().Element(0).Object().Value("observation").Equal("117.9")
+				response.Value("offset").Equal(0)
+				response.Value("total_observations").Equal(1)
 				response.Value("unit_of_measure").Equal("Pounds Sterling")
 			})
 		})
@@ -89,19 +90,73 @@ func TestSuccessfullyGetObservationForVersion(t *testing.T) {
 					WithHeader(florenceTokenName, florenceToken).
 					Expect().Status(http.StatusOK).JSON().Object()
 
-				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Length().Equal(1)
-				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Element(0).Object().Value("href").String().Match("/codelists/508064B3-A808-449B-9041-EA3A2F72CFAD/codes/cpi1dim1S40403$")
-				response.Value("dimensions").Object().Value("aggregate").Object().Value("options").Array().Element(0).Object().Value("id").Equal("cpi1dim1S40403")
-				response.Value("dimensions").Object().Value("geography").Object().Value("options").Array().Length().Equal(1)
-				response.Value("dimensions").Object().Value("geography").Object().Value("options").Array().Element(0).Object().Value("href").String().Match("/codelists/708064B3-A808-449B-9041-EA3A2F72CFAF/codes/K02000001$")
-				response.Value("dimensions").Object().Value("geography").Object().Value("options").Array().Element(0).Object().Value("id").Equal("K02000001")
-				response.Value("dimensions").Object().Value("time").Object().Value("options").Array().Length().Equal(1)
-				response.Value("dimensions").Object().Value("time").Object().Value("options").Array().Element(0).Object().Value("href").String().Match("/codelists/608064B3-A808-449B-9041-EA3A2F72CFAE/codes/Aug-16$")
-				response.Value("dimensions").Object().Value("time").Object().Value("options").Array().Element(0).Object().Value("id").Equal("Aug-16")
-				response.Value("observation").Equal("154.6")
+				response.Value("dimensions").Object().Value("aggregate").Object().Value("option").Object().Value("href").String().Match("/codelists/508064B3-A808-449B-9041-EA3A2F72CFAD/codes/cpi1dim1S40403$")
+				response.Value("dimensions").Object().Value("aggregate").Object().Value("option").Object().Value("id").Equal("cpi1dim1S40403")
+				response.Value("dimensions").Object().Value("geography").Object().Value("option").Object().Value("href").String().Match("/codelists/708064B3-A808-449B-9041-EA3A2F72CFAF/codes/K02000001$")
+				response.Value("dimensions").Object().Value("geography").Object().Value("option").Object().Value("id").Equal("K02000001")
+				response.Value("dimensions").Object().Value("time").Object().Value("option").Object().Value("href").String().Match("/codelists/608064B3-A808-449B-9041-EA3A2F72CFAE/codes/Aug-16$")
+				response.Value("dimensions").Object().Value("time").Object().Value("option").Object().Value("id").Equal("Aug-16")
+				response.Value("limit").Equal(10000)
 				response.Value("links").Object().Value("dataset_metadata").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/2/metadata$")
 				response.Value("links").Object().Value("self").Object().Value("href").String().Match(".+/datasets/" + datasetID + "/editions/" + edition + "/versions/2/observations\\?aggregate=cpi1dim1S40403&geography=K02000001&time=Aug-16$")
+				response.Value("links").Object().Value("version").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/2$")
 				response.Value("links").Object().Value("version").Object().Value("id").Equal("2")
+				response.Value("observations").Array().Length().Equal(1)
+				response.Value("observations").Array().Element(0).Object().Value("observation").Equal("154.6")
+				response.Value("offset").Equal(0)
+				response.Value("total_observations").Equal(1)
+				response.Value("unit_of_measure").Equal("Pounds Sterling")
+			})
+		})
+
+		Convey("When a request is made to get an observations resource containing more than one observation for an unpublished version", func() {
+			Convey("Then the response body contains the expected observations data", func() {
+				response := datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
+					WithQueryString("time=Aug-16&geography=K02000001&aggregate=*").
+					WithHeader(florenceTokenName, florenceToken).
+					Expect().Status(http.StatusOK).JSON().Object()
+
+				response.Value("dimensions").Object().Value("geography").Object().Value("option").Object().Value("href").String().Match("/codelists/708064B3-A808-449B-9041-EA3A2F72CFAF/codes/K02000001$")
+				response.Value("dimensions").Object().Value("geography").Object().Value("option").Object().Value("id").Equal("K02000001")
+				response.Value("dimensions").Object().Value("time").Object().Value("option").Object().Value("href").String().Match("/codelists/608064B3-A808-449B-9041-EA3A2F72CFAE/codes/Aug-16$")
+				response.Value("dimensions").Object().Value("time").Object().Value("option").Object().Value("id").Equal("Aug-16")
+				response.Value("limit").Equal(10000)
+				response.Value("links").Object().Value("dataset_metadata").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/2/metadata$")
+				response.Value("links").Object().Value("self").Object().Value("href").String().Match(".+/datasets/" + datasetID + "/editions/" + edition + "/versions/2/observations\\?aggregate=\\%2A&geography=K02000001&time=Aug-16$")
+				response.Value("links").Object().Value("version").Object().Value("href").String().Match("/datasets/" + datasetID + "/editions/" + edition + "/versions/2$")
+				response.Value("links").Object().Value("version").Object().Value("id").Equal("2")
+				response.Value("observations").Array().Length().Equal(137)
+
+				// check two observations in observations array
+				for i := 0; i < len(response.Value("observations").Array().Iter()); i++ {
+					count := 0
+					if response.Value("observations").Array().Element(i).Object().Value("dimensions").Object().Value("Aggregate").Object().Value("id").String().Raw() == "cpi1dim1S10107" {
+						observation := response.Value("observations").Array().Element(i).Object()
+						observation.Value("dimensions").Object().Value("Aggregate").Object().Value("href").String().Match("/codelists/508064B3-A808-449B-9041-EA3A2F72CFAD/codes/cpi1dim1S10107")
+						observation.Value("dimensions").Object().Value("Aggregate").Object().Value("id").Equal("cpi1dim1S10107")
+						observation.Value("dimensions").Object().Value("Aggregate").Object().Value("label").Equal("01.1.7 Vegetables including potatoes and tubers")
+						observation.Value("dimensions").Object().NotContainsKey("geography")
+						observation.Value("observation").Equal("136.8")
+						count++
+					}
+
+					if response.Value("observations").Array().Element(i).Object().Value("dimensions").Object().Value("Aggregate").Object().Value("id").String().Raw() == "cpi1dim1G100000" {
+						observation := response.Value("observations").Array().Element(i).Object()
+						observation.Value("dimensions").Object().Value("Aggregate").Object().Value("href").String().Match("/codelists/508064B3-A808-449B-9041-EA3A2F72CFAD/codes/cpi1dim1G100000")
+						observation.Value("dimensions").Object().Value("Aggregate").Object().Value("id").Equal("cpi1dim1G100000")
+						observation.Value("dimensions").Object().Value("Aggregate").Object().Value("label").Equal("10.0 Education")
+						observation.Value("dimensions").Object().NotContainsKey("geography")
+						observation.Value("observation").Equal("244.3")
+						count++
+					}
+
+					if count == 2 {
+						break
+					}
+				}
+
+				response.Value("offset").Equal(0)
+				response.Value("total_observations").Equal(137)
 				response.Value("unit_of_measure").Equal("Pounds Sterling")
 			})
 		})
@@ -187,7 +242,7 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 
 	Convey("Given a published dataset exist", t, func() {
 		if err := mongo.Setup(datasetDoc); err != nil {
-			log.ErrorC("Was unable to run test", err, nil)
+			log.ErrorC("Unable to set up published dataset doc", err, nil)
 			os.Exit(1)
 		}
 
@@ -206,7 +261,7 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 		Convey("and a published edition exist", func() {
 
 			if err := mongo.Setup(publishedEditionDoc); err != nil {
-				log.ErrorC("Was unable to run test", err, nil)
+				log.ErrorC("Unable to set up published edition doc", err, nil)
 				os.Exit(1)
 			}
 
@@ -225,7 +280,7 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 			Convey("and an unpublished version exist", func() {
 
 				if err := mongo.Setup(unpublishedVersionDoc); err != nil {
-					log.ErrorC("Was unable to run test", err, nil)
+					log.ErrorC("Unable to set up unpublished version doc", err, nil)
 					os.Exit(1)
 				}
 
@@ -249,6 +304,16 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 					})
 				})
 
+				Convey("When a request to get an observation for a version of a dataset with more than one wildcard used", func() {
+					Convey("Then return status bad request (400) with a message", func() {
+						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
+							WithHeader(florenceTokenName, florenceToken).
+							WithQueryString("time=*&geography=*&aggregate=cpi1dim1S40403").
+							Expect().Status(http.StatusBadRequest).
+							Body().Contains("only one wildcard (*) is allowed as a value in selected query parameters")
+					})
+				})
+
 				Convey("When a request to get an observation for an unpublished version of a dataset with the correct query parameters but the values don't exist", func() {
 					Convey("Then return status not found (404) with message `Observation not found`", func() {
 						datasetAPI.GET("/datasets/{id}/editions/{edition}/versions/2/observations", datasetID, edition).
@@ -262,7 +327,7 @@ func TestFailureToGetObservationForVersion(t *testing.T) {
 		})
 
 		if err := mongo.Teardown(datasetDoc, publishedEditionDoc, unpublishedVersionDoc); err != nil {
-			log.ErrorC("Was unable to run test", err, nil)
+			log.ErrorC("Unable to teardown docs: datasetDoc, publishedEditionDoc, unpublishedVersionDoc", err, nil)
 			os.Exit(1)
 		}
 	})
