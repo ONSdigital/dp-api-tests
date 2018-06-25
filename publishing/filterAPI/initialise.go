0375e95a61a8b769f4010050ee45abce0218acee
mikeAdams <mikelovesbooks@gmail.com>
2018-06-25 15:31:45 +0100

    Reapplied original auth-stub key

--- publishing/filterAPI/initialise.go
+++ publishing/filterAPI/initialise.go
@@ -8,23 +8,21 @@ import (
 	"github.com/ONSdigital/go-ns/log"
 )
 
-var (
-	cfg *config.Config
-	serviceAuthToken = "Bearer " + os.Getenv("SERVICE_AUTH_TOKEN")
-)
+var cfg *config.Config
 
 const (
 	collection = "filters"
 
 	serviceAuthTokenName    = "Authorization"
+	serviceAuthToken        = "Bearer FD0108EA-825D-411C-9B1D-41EF7727F465"
 	downloadServiceToken    = "QB0108EZ-825D-412C-9B1D-41EF7747F462"
 	invalidServiceAuthToken = "invalid-auth-token"
 
-	filterNotFoundResponse = "filter blueprint not found\n"
+	filterNotFoundResponse       = "filter blueprint not found\n"
 	filterOutputNotFoundResponse = "filter output not found\n"
-	versionNotFoundResponse = "version not found\n"
-	dimensionNotFoundResponse = "dimension not found\n"
-	optionNotFoundResponse = "option not found\n"
+	versionNotFoundResponse      = "version not found\n"
+	dimensionNotFoundResponse    = "dimension not found\n"
+	optionNotFoundResponse       = "option not found\n"
 )
 
 func init() {
