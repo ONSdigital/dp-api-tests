da49482b3f766763487e3283280c69988b188a8f
mattrout92 <matt_rout92@live.co.uk>
2018-07-06 14:06:12 +0100

    Remove ID field on edition response and split 404 tests into separate cases

--- codeListAPI/get_edition_test.go
+++ codeListAPI/get_edition_test.go
@@ -22,12 +22,11 @@ func TestSuccessfulRetrievalOfEdition(t *testing.T) {
 
 	codelistAPI := httpexpect.New(t, cfg.CodeListAPIURL)
 
-	Convey("Given an edition of a codelist eqists", t, func() {
+	Convey("Given an edition of a codelist exists", t, func() {
 		Convey("When a request is made for the edition", func() {
 			Convey("Then the codelist is returned to the user", func() {
 				response := codelistAPI.GET("/code-lists/ABCDEF/editions/2018").Expect().Status(http.StatusOK).JSON().Object()
 
-				response.Value("id").Equal("ABCDEF")
 				response.Value("edition").Equal("2018")
 				response.Value("label").Equal("Tottenham")
 				response.Value("links").Object().Value("self").Object().Value("href").Equal("/code-lists/ABCDEF/editions/2018")
@@ -44,6 +43,14 @@ func TestSuccessfulRetrievalOfEdition(t *testing.T) {
 			Convey("Then the a not found status is returned to the user", func() {
 
 				codelistAPI.GET("/code-lists/BCDSOE/editions/2018").Expect().Status(http.StatusNotFound)
+
+			})
+		})
+	})
+
+	Convey("Given a user wants an edition which doesn't exist for a valid code list", t, func() {
+		Convey("When a request is made for the edition", func() {
+			Convey("Then the a not found status is returned to the user", func() {
 
 				codelistAPI.GET("/code-lists/ABCDEF/editions/2019").Expect().Status(http.StatusNotFound)
 
--- codeListAPI/get_editions_test.go
+++ codeListAPI/get_editions_test.go
@@ -24,13 +24,12 @@ func TestSuccessfulRetrievalOfEditions(t *testing.T) {
 
 	Convey("Given two editions of the same codelist exists", t, func() {
 		Convey("When a request is made for the editions", func() {
-			Convey("Then the codelists are returned to the user", func() {
+			Convey("Then the editions for the codelist are returned to the user", func() {
 				response := codelistAPI.GET("/code-lists/ABCDEF/editions").Expect().Status(http.StatusOK).JSON().Object()
 
 				response.Value("number_of_results").Equal(2)
 
 				item1 := response.Value("items").Array().Element(0).Object()
-				item1.Value("id").Equal("ABCDEF")
 				item1.Value("edition").Equal("2018")
 				item1.Value("label").Equal("Tottenham")
 				item1.Value("links").Object().Value("self").Object().Value("href").Equal("/code-lists/ABCDEF/editions/2018")
@@ -39,7 +38,6 @@ func TestSuccessfulRetrievalOfEditions(t *testing.T) {
 				item1.Value("links").Object().Value("codes").Object().Value("href").Equal("/code-lists/ABCDEF/editions/2018/codes")
 
 				item2 := response.Value("items").Array().Element(1).Object()
-				item2.Value("id").Equal("ABCDEF")
 				item2.Value("edition").Equal("2017")
 				item2.Value("label").Equal("Tottenham")
 				item2.Value("links").Object().Value("self").Object().Value("href").Equal("/code-lists/ABCDEF/editions/2017")
