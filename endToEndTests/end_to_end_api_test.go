6758120bc94c4825db52a5e306d96068ad373ac9
Eleanor Deal <eleanor.deal@gmail.com>
2018-07-23 14:43:54 +0100

    Increase timeout for end to end test again

--- endToEndTests/end_to_end_api_test.go
+++ endToEndTests/end_to_end_api_test.go
@@ -24,7 +24,7 @@ import (
 	"github.com/ONSdigital/go-ns/rchttp"
 )
 
-var timeout = time.Duration(60 * time.Second)
+var timeout = time.Duration(120 * time.Second)
 
 func TestSuccessfulEndToEndProcess(t *testing.T) {
 
